1ac9b427d2fec1e85c8b0a1fb5c8720d0e8fdbf7
ggeo <geovannesilvadejesus13@gmail.com>
2024-10-29 17:19:05 -0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,2 +1,15 @@
-echo 0 > /proc/sys/vm/overcommit_memory
-echo 1 > /proc/sys/vm/oom_kill_allocating_task
+sleep 25
+#this is for system apps
+echo '100' > /dev/memcg/system/memory.swappiness
+#this is for user applications
+echo '50' > /dev/memcg/apps/memory.swappiness
+#disable the use of Threads used by zram
+echo 0 > /sys/block/zram0/max_comp_streams
+#this can reduce pressure on RAM memory
+echo 50 > /proc/sys/vm/vfs_cache_pressure
+#Set the maximum percentage of RAM that can be used as write cache before forcing writing to disk.
+echo 10 > /proc/sys/vm/dirty_ratio
+#Limita o percentual da RAM usado antes de iniciar a escrita de cache em segundo plano.
+echo 5 > /proc/sys/vm/dirty_background_ratio
+#change zram to lz4
+echo lz4 > /sys/block/zram0/comp_algorithm
